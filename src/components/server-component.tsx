65d7feb0c00417e2efa9596a250fbd49c53bfc64
Andres Sanabria <sanabria80.andres@gmail.com>
2024-05-01 10:21:03 -0500

    feat: restore files

--- src/components/server-component.tsx
+++ src/components/server-component.tsx
@@ -1,10 +1,6 @@
+import getData from "../utils/getData";
 export default async function ServerComponent(){
-    const simulateAsyncRequest = new Promise((resolve) => {
-        setTimeout(() => {
-            resolve('Datos recibidos');
-        }, 4000);
-    });
-    await simulateAsyncRequest;
+    const data = await getData();
     return (
         <div className="bg-green-100 p-4 rounded-md">
             <h1>Server Component Loaded</h1>
